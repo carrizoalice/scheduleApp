779a4cd75dc04d3f36c39985fa3ade4e387865ac
Alice <carrizoalice@gmail.com>
2020-12-05 20:46:53 -0300

    date

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var date = new Date();
+var months = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+
+var divDate = document.querySelector('.day');
+var divMonthYear = document.querySelector('.month-year');
+divDate.innerHTML = addZero(date.getDate());
+divMonthYear.innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
+// var dd = date.getDate();
+// var mm = date.getMonth()+1;
+// var yyyy = date.getFullYear();
+
+
+
+
+
+function addZero(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
 
