742080ebdc93c45ae0178c747579de67ca082007
Alice <carrizoalice@gmail.com>
2020-12-05 21:29:09 -0300

    optimization code

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-var date = new Date();
-var months = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+const date = new Date();
+const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"];
 
-var divDate = document.querySelector('.day');
-var divMonthYear = document.querySelector('.month-year');
-divDate.innerHTML = addZero(date.getDate());
-divMonthYear.innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
-// var dd = date.getDate();
-// var mm = date.getMonth()+1;
-// var yyyy = date.getFullYear();
-
-
-
+document.querySelector('.day').innerHTML = addZero(date.getDate());
+document.querySelector('.month-year').innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
 
 
 function addZero(i) {
