4b39642a096ceb6c7776bf6764ca5a2d4dcf5f41
Alice <carrizoalice@gmail.com>
2020-12-17 22:17:04 -0300

    setDate

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const date = new Date();
-const months = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre"];
+    const date = new Date();
+    const months = [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"];
 
-document.querySelector('.day').innerHTML = addZero(date.getDate());
-document.querySelector('.month-year').innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
+    document.querySelector('.day').innerHTML = addZero(date.getDate());
+    document.querySelector('.month-year').innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
 
 function addZero(i) {
     if (i < 10) {
@@ -25,20 +25,18 @@ function addZero(i) {
 
 function createEvent() {
     var newEvent = document.getElementById('text').value;    
-    var newDate = new Date();
-    putEvent(newEvent);
-    return newEvent;
-    
+    var newDate = new Date();    
+    putEvent(newEvent, newDate);
+    return newEvent;    
 }
 
-function putEvent(newEvent) {
+function putEvent(newEvent, newDate) {
     var putEvent = document.getElementById('put-event');
-    putEvent.innerHTML = newEvent;
+    putEvent.innerHTML = '<div>' + newDate.getDate()  + "-" + (newDate.getMonth()+1) + "-" + newDate.getFullYear() + '<br>'+ newEvent +'</div>';
 }
 
 btnSend.addEventListener('click', function(event){  
-    createEvent();
-    
+    createEvent();    
 })
 
 
